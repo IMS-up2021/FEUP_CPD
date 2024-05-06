c501d1bed50a43e406a1d26f2882b05ae543e896
joanamaia03 <93883152+joanamaia03@users.noreply.github.com>
2024-05-06 20:11:37 +0100

    multiplayer done

---
+++ assign2/src/src/Main.java
@@ -0,0 +1,10 @@
+import java.util.concurrent.atomic.AtomicInteger;
+
+class Main{
+    public static void main(String[] args) {
+        Game game = new Game();
+        int number_players = 3;
+        game.wordle(number_players);
+    }
+}
+
