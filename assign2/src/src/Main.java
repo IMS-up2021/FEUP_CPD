f169f9994da259eb96b37e8647e722371ea09f85
Inês <ines.soares.22102003@gmail.com>
2024-05-14 09:36:20 +0100

    Main

--- assign2/src/src/Main.java
+++ assign2/src/src/Main.java
@@ -1,11 +1,12 @@
+import java.util.Objects;
+import java.util.Scanner;
 import java.util.concurrent.atomic.AtomicInteger;
-
 class Main{
-    public static void main(String[] args) {
-        //Server server = new Server();
-        Game game = new Game();
-        int number_players = 3;
-        game.wordle(number_players);
+    public static void main(String[] args) throws Exception {
+        Server server = new Server();
+        server.getWords("assign2/resources/words.txt");
+        int count = Player.count;
+        server.startGame(count);
     }
 }
 
